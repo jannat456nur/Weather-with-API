b7e52f3d8e80065730804ab4f84b4f7cf4c6e05d
jannat456nur <jannatjenny22@gmail.com>
2021-09-02 18:59:10 -0700

    first commit

---
+++ js/app2.js
@@ -0,0 +1,25 @@
+const API_KEY = `884ce6aab1f6cb89dcdf9d66e87d6261`;
+
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    console.log(url)
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data))
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name)
+    setInnerText('temperature', temperature.main.temp)
+    setInnerText('condition', temperature.weather[0].main)
+    consolel.log(temperature)
+    const url = ` http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcion = document.getElementById('weather-icon')
+    imgIcion.setAttribute('src', url)
+}
